311acbef001e9222ce2f3741d1bc33898820ce61
rosario <rosario.duenas@pixtig.com>
2022-09-04 16:55:53 -0600

    tarea

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"   
+import { crud_cliente } from "./controlador/crud_clientes.js";
+const app_e = express();
+app_e.use(express.urlencoded({extended:false}));
+app_e.use(express.json());
+app_e.use(express.static('./vista'))
+app_e.use(express.static('./controlador'))
+app_e.use(express.static('./modelo'))
+//motor de vistas
+app_e.set('views','./vista')
+app_e.set('view engine','ejs')
+app_e.listen('5000',function(){
+    console.log('Apliacion iniciada en: http://localhost:5000/ ')
+})
+app_e.get('/',crud_cliente.leer);
+app_e.post('/crud_c',crud_cliente.cud);
